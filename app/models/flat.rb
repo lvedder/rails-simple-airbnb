76cc6cd6ce31b0d46e7f0ed3ec6c2ce98932134a
Lamina Vedder <lvedder@mail.uni-mannheim.de>
2018-11-22 09:09:18 +0100

    first version of my simple Airbnb

---
+++ app/models/flat.rb
@@ -0,0 +1,5 @@
+class Flat < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :price_per_night, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating restaurants..."
+flat_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Spacious Apartment in Center of Barcelona',
+    address: 'Eixample Barcelona',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa culpa harum laudantium sint architecto magni et voluptates dicta',
+    price_per_night: 110,
+    number_of_guests: 4
+  },
+  {
+    name: 'Bright Flat Next to the Rhine River',
+    address: 'Taunusstraße 45, 55118 Mainz',
+    description: 'Enjoy the view on the beautiful Rhine River of Mainz and enjoy the sunrise straight out of bed',
+    price_per_night: 25,
+    number_of_guests: 2
+  },
+  {
+    name: 'Luxurious apartment in the heart of Bangkok',
+    address: 'Sukhumvit Soi 21, 10000 Bangkok',
+    description: 'Lively area with a lot of nice restaurants and pubs to hang out at night',
+    price_per_night: 35,
+    number_of_guests: 2
+  }
+]
+Flat.create!(flat_attributes)
+puts "Finished!"
